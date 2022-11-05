670778103338325c85d1f349738f41729bf90521
mohammed-r-mohsen <mohmohsen20012@gmail.com>
2022-11-05 03:11:28 -0700

    route
    Signed-off-by: mohammed-r-mohsen <mohmohsen20012@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// basic route 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('deafult');
+
+Route::get('/test' , function()
+{
+    return view('test');
+})->name('basic');
+
+// route parameeters 
+
+Route::get('/test2/{name}' , function($name='user'){
+    return $name ; 
+})->name('require-parameter');
+
+Route::get('/test3/{name?}' , function($name='user'){
+    return $name ; 
+})->name('optional-parameter');
+
+Route::get('/test4/{name}/{id?}' , function($name , $id = 12){
+    return "name : $name \n id : $id"; 
+})->name('rule-parameter');
+
+
